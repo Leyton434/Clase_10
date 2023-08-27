fb08527ab54db472685480d1ef4b8e55d612848e
Leyton434 <emanolpla2002@gmail.com>
2023-08-27 13:50:33 -0300

    Clase 10 js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+const nombre="Lolito";
+const apellido="FDEZ";
+
+console.log("el nombre y apellido del usuario son:" , nombre +" "+ apellido );
+
+const num1= 5;
+const num2= 10;
+const suma= num1+num2;
+
+console.log("el resultado de la suma es:" , suma);
+
+const otravar= suma+2 ;
+
+console.log("el resultado es:", otravar); 
+
